b3b883568aa12436c516122c925bc86211916965
Lisa Connell <connell.lisamarie@gmail.com>
2018-03-10 00:23:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ function currentLine(line) {
   
   for (let i = 0, l = line.length; i < l; i++) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`)
+ }
+ return `The line is currently: ${numbersAndNames.join(', ')}`
 }
