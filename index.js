4ac6740bb02e316fb1eb8b0701ba065d84d010c6
Lisa Connell <connell.lisamarie@gmail.com>
2018-03-10 00:21:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function currentLine(line) {
     return "The line is currently empty."
   }
   const numbersAndNames = []
+  
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
 }
